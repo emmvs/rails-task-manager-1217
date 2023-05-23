149e82f2b5101a57f1101124835d2e3cc58d4bcc
Emma A. A. Rünzel <emma@ruenzel.de>
2023-05-23 17:26:59 +0200

    Update Views of Tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -18,14 +18,17 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
     @task.update(task_params)
-    redirect_to task_path(@task)
+    redirect_to tasks_path
   end
 
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
     redirect_to tasks_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "tasks#index"
   get 'tasks', to: 'tasks#index'
   post 'tasks', to: 'tasks#create'
   get 'tasks/new', to: 'tasks#new', as: :new_task
   get 'tasks/:id', to: 'tasks#show', as: :task
   get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
   patch 'tasks/:id', to: 'tasks#update'
-  delete "/tasks/:id", to: "tasks#destroy", as: :delete_task
+  delete '/tasks/:id', to: 'tasks#destroy', as: :delete_task
 
   # resources :tasks
 end
