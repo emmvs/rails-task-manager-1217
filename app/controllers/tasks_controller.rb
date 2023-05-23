24bf3f4a78ed96d4403d9596f9ad85745c2d57fd
Emma A. A. Rünzel <emma@ruenzel.de>
2023-05-23 09:43:24 +0200

    Create Controller Actions

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,23 +1,36 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    task = Task.new(task_params)
+    task.save
+    redirect_to tasks_path
   end
 
   def edit
   end
 
   def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
   end
 
   def destroy
-    Task.find(params[:id]).destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  delete "/taks/:id", to: "tasks#destroy", as: :delete_task
+  get 'tasks', to: 'tasks#index'
+  post 'tasks', to: 'tasks#create'
+  get 'tasks/new', to: 'tasks#new', as: :new_task
+  get 'tasks/:id', to: 'tasks#show', as: :task
+  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  patch 'tasks/:id', to: 'tasks#update'
+  delete "/tasks/:id", to: "tasks#destroy", as: :delete_task
+
+  # resources :tasks
 end
